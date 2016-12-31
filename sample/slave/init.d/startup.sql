09f5cb9c2236a737a59e79832adf9a06b3e4b1e4
okasho <okasho1010@gmail.com>
2016-12-31 16:28:03 +0900

    最終調整

--- sample/slave/init.d/startup.sql
+++ sample/slave/init.d/startup.sql
@@ -1,6 +1,5 @@
 CHANGE MASTER TO MASTER_HOST='master_1', MASTER_USER='root',MASTER_PASSWORD='root', MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=120;
-GRANT ALL ON *.* TO root@'172.%' IDENTIFIED BY 'root' WITH GRANT OPTION;
-
+grant all privileges on *.* to root@'%' identified by 'root';
 
 FLUSH PRIVILEGES;
 SHOW SLAVE STATUS \G
